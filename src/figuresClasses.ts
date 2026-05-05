e89edf249bab9bafec4030a075d84f1ed763d070
valerij <valerijkolpacov@gmail.com>
2026-05-05 20:22:34 +0300

    ts_figures_classes

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -38,7 +38,7 @@ export class Triangle implements Figure {
     const p = (this.a + this.b + this.c) / 2;
     const area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
 
-    return Number(area.toFixed(2));
+    return Math.floor(area * 100) / 100;
   }
 }
 
@@ -77,7 +77,7 @@ export class Rectangle implements Figure {
   getArea(): number {
     const area = this.width * this.height;
 
-    return Number(area.toFixed(2));
+    return Math.floor(area * 100) / 100;
   }
 }
 
